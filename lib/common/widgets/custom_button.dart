8b38b3b7dc70f9f74cb44ae524954cc29265ca88
Nde Dilan <nde.dilan@ictuniversity.edu.cm>
2024-12-23 15:27:13 +0100

    feat:Added the button with cubit and all the state

--- lib/common/widgets/custom_button.dart
+++ lib/common/widgets/custom_button.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:other_screens/common/bloc/button/button_state.dart';
+import 'package:other_screens/common/bloc/button/button_state_cubit.dart';
 import 'package:other_screens/common/constants.dart';
 
 class CustomButton extends StatelessWidget {
@@ -15,6 +18,37 @@ class CustomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return BlocBuilder<ButtonStateCubit, ButtonState>(
+        builder: (context, state) {
+      print(state);
+
+      if (state is ButtonLoadingState) {
+        return _loading(context);
+      }
+      return _initial(context);
+    });
+  }
+
+  Widget _loading(BuildContext context) {
+    return ElevatedButton(
+        onPressed: null,
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(mediaWidth(context) / 0.9, mediaWidth(context) / 6),
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          disabledBackgroundColor: Colors.grey,
+        ),
+        child: Container(
+            height: 50,
+            alignment: Alignment.center,
+            child: const CircularProgressIndicator()));
+  }
+
+  Widget _initial(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
--- lib/presentation/pages/landing_page.dart
+++ lib/presentation/pages/landing_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:other_screens/common/constants.dart';
-import 'package:other_screens/common/widgets/custom_button.dart';
 import 'package:other_screens/presentation/auth/pages/login_page.dart';
 import 'package:other_screens/presentation/auth/pages/register_page.dart';
 
@@ -49,7 +48,7 @@ class LandingPage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 32.0),
-              CustomButton(
+              NextButton(
                 onPressed: () {
                   // Navigate to "Get Started" screen
                   Navigator.push(context,
@@ -58,7 +57,7 @@ class LandingPage extends StatelessWidget {
                 isEnabled: true,
                 text: 'GET STARTED',
               ),
-              CustomButton(
+              NextButton(
                 onPressed: () {
                   // Navigate to "LOG IN" screen
                   Navigator.push(context,
@@ -70,6 +69,44 @@ class LandingPage extends StatelessWidget {
               SizedBox(height: 32.0),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+ 
+
+class NextButton extends StatelessWidget {
+  final bool? isEnabled;
+  final String text;
+  final VoidCallback? onPressed;
+
+  const NextButton({
+    super.key,
+    this.isEnabled,
+    this.onPressed,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(mediaWidth(context) / 0.9, mediaWidth(context) / 6),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        disabledBackgroundColor: Colors.grey,
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
         ),
       ),
     );
